5a4850ed00a62e856659a21c5a49b847b52bcc27
Julian Sniffen <julian.sniffen@gmail.com>
2022-09-28 21:57:54 -0400

    go fmt

--- main.go
+++ main.go
@@ -1,78 +1,87 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
-  "time"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-  parse()
+	parse()
 }
 
 func generateFilename() string {
-  t := time.Now()
-  return fmt.Sprintf("%d%d%d-%d%d%d",
-    t.Year(), t.Month(), t.Day(),
-    t.Hour(), t.Minute(), t.Second())
+	t := time.Now()
+	return fmt.Sprintf("%d%d%d-%d%d%d",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
 }
 
 func createSnippet(s string) {
-  fn := generateFilename()
+	titleEnd := strings.Index(s, ":")
+	if titleEnd == -1 {
+		abort("Title required")
+	}
+	title := s[:titleEnd]
 
-  tags := make([]string, 0)
+	links := make([]string, 0)
+	for _, sub := range strings.Split(s, "[[") {
+		end := strings.Index(sub, "]]")
+		if end != -1 {
+			links = append(links, sub[:end])
+		}
+	}
 
-  for _, word := range strings.Split(s, " ") {
-    if strings.HasPrefix(word, "@") {
-      tags = append(tags, word)
-    }
-  }
 
-  fmt.Println("parsed tags", tags)
+	fmt.Println("parsed title", title)
+	fmt.Println("parsed links", links)
 
-  err := os.WriteFile(fn, []byte(s), 0777)
-  if err != nil {
-    fmt.Println("Error creating snippet", err)
-    os.Exit(1)
-  }
-  fmt.Println("created snippet at", fn)
-  os.Exit(0)
+	err := os.WriteFile(title, []byte(s[titleEnd+1:]), 0777)
+	if err != nil {
+		fmt.Println("Error creating snippet", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
 
 func help() {
-  fmt.Println("help")
-  os.Exit(0)
+	fmt.Println("help")
+	os.Exit(0)
+}
+
+func abort(s string) {
+	fmt.Println(s)
+	os.Exit(1)
 }
 
 func new() {
-  args := os.Args[2:]
+	args := os.Args[2:]
 
-  if len(args) == 0 {
-    fmt.Println("args required for new snippet")
-    os.Exit(0)
-  }
+	if len(args) == 0 {
+		fmt.Println("args required for new snippet")
+		os.Exit(0)
+	}
 
-  s := strings.Join(args, " ")
-  createSnippet(s)
+	s := strings.Join(args, " ")
+	createSnippet(s)
 }
 
 func search() {
-  fmt.Println("search")
+	fmt.Println("search")
 }
 
 func parse() {
-  if len(os.Args) < 2 {
-    help()
-  }
+	if len(os.Args) < 2 {
+		help()
+	}
 
-  arg := strings.ReplaceAll(os.Args[1], "-", "")
-  if arg == "n" || arg == "new" {
-    new()
-  } else if arg == "s" || arg == "search" {
-    search()
-  } else {
-    help()
-  }
+	arg := strings.ReplaceAll(os.Args[1], "-", "")
+	if arg == "n" || arg == "new" {
+		new()
+	} else if arg == "s" || arg == "search" {
+		search()
+	} else {
+		help()
+	}
 }
-
